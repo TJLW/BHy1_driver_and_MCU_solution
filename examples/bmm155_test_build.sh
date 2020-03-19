9e3e6ff9394213c81aa984824caa157fe8a348fb
TJLW <TJLWhitaker@gmail.com>
2020-03-19 15:21:36 -0500

    Update bmm155_test_build.sh

--- examples/bmm155_test_build.sh
+++ examples/bmm155_test_build.sh
@@ -1,1 +1,1 @@
-gcc rotation_vector_example.c ../driver/src/* -I ../driver/inc -I/usr/include/glib-2.0 -lm -lglib-2.0 -o bmm15_test -g
+gcc rotation_vector_example.c ../driver/src/* -I ../driver/inc -lm -o bmm15_test -g
